031fbb19c9f125b02800c0781acf5818e5d15b44
MilanK1 <54515638+MilanK1@users.noreply.github.com>
2021-08-03 10:17:23 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+  import logo from './logo.svg';
 import './App.css';
-
+import websocketWrapper from "react-use-websocket";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+  <ul id="messages"></ul>
+  <div id="form">
+    <input id="input" /><button>Send</button>
+  </div>
     </div>
   );
 }
